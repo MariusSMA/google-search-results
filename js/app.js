8b7b753e17d0bbdce5dc30aaa562eb25e38dc776
Marius S <alexwtfmarius@gmail.com>
2021-08-20 22:19:38 +0300

    added a new accordion section to the project, fixed some minor bugs

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
+// Menu
+
 const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu");
 const menuBackground = document.querySelector(".menu-bg");
-const expandIcon = document.querySelector(".expand-icon");
-const itemBody = document.querySelector(".search-card-body-item-container");
-const itemBodyItem = document.querySelector(".search-card-body-item");
-const itemHeader = document.querySelector(".search-card-body-item-header");
 
 menuIcon.addEventListener("click", () => {
   menu.classList.toggle("menu--active");
@@ -16,15 +14,32 @@ menuBackground.addEventListener("click", () => {
   menuBackground.classList.remove("menu-bg--active");
 });
 
-expandIcon.addEventListener("click", () => {
-  expandIcon.classList.toggle("expand-icon--active");
-  itemBody.classList.toggle("search-card-body-item-container--active");
-  itemBodyItem.style.padding = "0.35rem 0";
-  itemHeader.innerHTML += `
-      <div style="display: flex; align-items: center; width: 40px; height: 40px; order: -1; background-color: rgba(0,0,0,.03); border-radius: 0.25rem; margin-right: 0.625rem;">
-        <img style="width: 40px;" src="../images/go-daddy-small-logo.png" alt="GoDaddy Logo"/>
-      </div>
-    `;
+// People also ask
+
+const expandMoreIcon = document.querySelector(".expand-more-icon");
+const itemCardContainer = document.querySelector(
+  ".accordion-item-card-container"
+);
+
+expandMoreIcon.addEventListener("click", () => {
+  expandMoreIcon.classList.toggle("expand-more-icon--active");
+  itemCardContainer.classList.toggle("accordion-item-card-container--active");
 });
 
-// @TODO place the img and the img container to the html with display none and then create classes and toggle them np ok bro luv <3
+// Related searches
+
+const expandIcon = document.querySelector(".expand-icon");
+const itemBody = document.querySelector(".search-card-item-container");
+const itemBodyList = document.querySelector(".search-card-item");
+const imgContainer = document.querySelector(
+  ".search-card-item-header-img-container"
+);
+
+expandIcon.addEventListener("click", () => {
+  expandIcon.classList.toggle("expand-icon--active");
+  itemBody.classList.toggle("search-card-item-container--active");
+  itemBodyList.classList.toggle("search-card-item--active");
+  imgContainer.classList.toggle(
+    "search-card-item-header-img-container--active"
+  );
+});

c10b872daafee71192f215de5bfa0245bc1ad776
Marius S <alexwtfmarius@gmail.com>
2021-08-22 00:37:37 +0300

    fixed some minor things, added videos section

--- js/app.js
+++ js/app.js
@@ -16,18 +16,24 @@ menuBackground.addEventListener("click", () => {
 
 // People also ask
 
-const expandMoreIcon = document.querySelector(".expand-more-icon");
-const itemCardContainer = document.querySelector(
+const askItemHeader = document.querySelectorAll(".accordion-item-header");
+const expandMoreIcon = document.querySelectorAll(".expand-more-icon");
+const itemCardContainer = document.querySelectorAll(
   ".accordion-item-card-container"
 );
 
-expandMoreIcon.addEventListener("click", () => {
-  expandMoreIcon.classList.toggle("expand-more-icon--active");
-  itemCardContainer.classList.toggle("accordion-item-card-container--active");
-});
+for (let i = 0; i < askItemHeader.length; i++) {
+  askItemHeader[i].addEventListener("click", () => {
+    expandMoreIcon[i].classList.toggle("expand-more-icon--active");
+    itemCardContainer[i].classList.toggle(
+      "accordion-item-card-container--active"
+    );
+  });
+}
 
 // Related searches
 
+const searchItemHeader = document.querySelector(".search-card-item-header");
 const expandIcon = document.querySelector(".expand-icon");
 const itemBody = document.querySelector(".search-card-item-container");
 const itemBodyList = document.querySelector(".search-card-item");
@@ -35,7 +41,7 @@ const imgContainer = document.querySelector(
   ".search-card-item-header-img-container"
 );
 
-expandIcon.addEventListener("click", () => {
+searchItemHeader.addEventListener("click", () => {
   expandIcon.classList.toggle("expand-icon--active");
   itemBody.classList.toggle("search-card-item-container--active");
   itemBodyList.classList.toggle("search-card-item--active");
